1f0d34ef38beadfdda6cd6371d1ec36b1a132cdd
Muskaan Jain <muskaansmn982@gmail.com>
2024-06-02 22:12:00 +0530

    Update String_Reversal.c

--- EMBEDDEDC/String_Reversal.c
+++ EMBEDDEDC/String_Reversal.c
@@ -11,8 +11,21 @@ int get_string_length(const char* str) {
     }   
     return count;
 }
+void reverse_word(char str[],int len)
+{
+    char temp=0;
+    int i=0;
+    while(i<len)
+    {
+        temp=str[i];
+        str[i]=str[len];
+        str[len]=temp;
+        i++;
+        len--;
+    }
+}
 
-void reverse_word(char str[],int j,int k)
+void reverse_words(char str[],int j,int k)
 {
     char temp=0;
 
@@ -26,7 +39,7 @@ void reverse_word(char str[],int j,int k)
     }
 }
 int main()
-{
+{  char str1[] = "Bangalore";
    char str[]="I am awesome";
   //main pointer
    int i=0;
@@ -34,23 +47,27 @@ int main()
    int j=0;
   //last pointer (length)
    int k =get_string_length(str)-1;
+   int len =get_string_length(str1)-1;
    printf("Org str: %s\n",str);
+   printf("Org str1: %s\n",str1);
     while(str[i]!='\0')
    {
        if (str[i]==' ')
        {
            k=i-1;
-           reverse_word(str,j,k);
+           reverse_words(str,j,k);
            j=i+1;//next word pointer 
     }
 
       ++i;
    }
    //Reverse last word
-   reverse_word(str, j, i - 1);
+   reverse_words(str, j, i - 1);
    //Reverse whole sentence
-   reverse_word(str, 0, get_string_length(str) - 1);
-
+   reverse_words(str, 0, get_string_length(str) - 1);
+  
+   reverse_word(str1,len);
+   printf("Rev str1: %s\n",str1);
   printf("Reversed sentence: %s\n", str);
 
   
